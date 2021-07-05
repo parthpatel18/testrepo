87ff331a8c565f08ed47919c70c7b08ced404da4
Parth Patel <parthpatel199618@gmail.com>
2021-07-05 03:35:54 +0000

    changes made from theia lab environment

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,5 @@
 # Display the output
 
 print("New Python File")
+
+print("This line was added from GitHub Lab using terminal")
---
+++ test_terminal.py
@@ -0,0 +1,3 @@
+# Adding a new file to a cloned repo
+
+print("This is a test file from a cloned repo")
